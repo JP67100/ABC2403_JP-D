cdef41ed5fa99074134c3219657e684d03b164cc
JP67100 <166002188+JP67100@users.noreply.github.com>
2024-04-25 12:52:32 +0200

    Exercice rayon

---
+++ 03-algorithmes/Exercice 1.4 Rayon/src/App.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		double Taille rayon d'un cercle;
+		double Aire Secteur Circulaire; 
+		double Volume;
+		
+		Scanner sc;
+		
+		sc = new Scanner (System.in);
+		
+		System.out.println(" Taille rayon d'un cercle: ");
+	
+		rayon = sc.nextDouble();
+		
+		Aire Secteur Circulaire = ( " 4 * 3.14 * Taille rayon d'un cercle " );
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+		
+		
+		
+		
+				
+	
+				
+				
+		
+		
+			
+		}
