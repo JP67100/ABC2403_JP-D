f0daf7a7fe784bb30974f67bd4d750851971a89f
JP67100 <166002188+JP67100@users.noreply.github.com>
2024-06-11 15:51:44 +0200

    test bouteille

--- 03-algorithmes/Bouteille/src/bouteille/App.java
+++ 03-algorithmes/Bouteille/src/bouteille/App.java
@@ -9,7 +9,14 @@ public class App {
 		Bouteille bouteille2 = new Bouteille(1.2f, 0.8f, true, "h2o");
 
 		Bouteille bouteille3 = new Bouteille(bouteille2);
+		
+		Bouteille bouteille4 = new Bouteille(1.2f, 0.5f, true, "h2o");
 
+		Bouteille bouteille5 = new Bouteille(1.2f, 1.3f, false, "h2o");
+		
+		Bouteille bouteille6 = new Bouteille(1.2f, 0.6f, true, "h2o" );
+		
+		
 		
 		
 		float contenanceEnCLDeBouteille2 = bouteille2.donneContenanceEnCL();
@@ -23,6 +30,14 @@ public class App {
 		boolean bouteilleRempli = bouteille2.laRemplir(0.3f);
 		
 		boolean bouteillevide = bouteille2.laVider(0.8f);
+		
+		boolean ContenuTrop = bouteille5.laRemplir (0.7f);
+		
+		boolean Retirer= bouteille5.laVider(0.4f);
+		
+		
+		
+		
 	}
 
 }
